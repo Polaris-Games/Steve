df85e3cba93d51a9682cd0ea8d145bf1d70aa647
OceanOSdev <ThomasTNF@live.com>
2015-09-20 21:01:34 -0500

    Changed the way rendering is done based on input

--- src/com/polaris/steve/GUIMainMenu.java
+++ src/com/polaris/steve/GUIMainMenu.java
@@ -8,20 +8,30 @@ import com.polaris.engine.Pos;
 import com.polaris.engine.Renderer;
 import static com.polaris.engine.Renderer.*;
 import static org.lwjgl.opengl.GL11.*;
+
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
 public class GUIMainMenu extends GUI{
 
-	Pos ballPos = new Pos(0,0);	// position of the ball
-	double XAccel = 1;
+	Pos ballPos = new Pos(1,-10.5);	// position of the ball
+	double XAccel = 1;	// acceleration multiplier
+	double YAccel = 1; // y accel
+	boolean needToSlowDown = false;		// quick workaround for momentum
+	// Math.atan(ballPos.getY() / ballPos.getX()) * 180 / Math.PI should equal the angle
 	public GUIMainMenu(Application app) {
 		super(app);
-		
 	}
 	
 	@Override
 	public void render(double x, double y, double delta){
-		gl3d();
-		glTranslated(0, 0,-10.5);
-		glRotated(ballPos.getX(), 10, 1, 1);
+		gl3d();	
+		
+		//glTranslated(0, 0,-10.5);
+		glTranslated(ballPos.getX(),0,ballPos.getY());
+		glRotated(45, 1, 1, 1);
+		
+	    
+		
 		glBegin();
 		glColor3f(0.0f,1.0f,0.0f);    // Color Blue
 	    glVertex3f( 1.0f, 1.0f,-1.0f);    // Top Right Of The Quad (Top)
@@ -54,7 +64,8 @@ public class GUIMainMenu extends GUI{
 	    glVertex3f( 1.0f,-1.0f, 1.0f);    // Bottom Left Of The Quad (Right)
 	    glVertex3f( 1.0f,-1.0f,-1.0f);    // Bottom Right Of The Quad (Right)
 	    glEnd();            // End Drawing The Cube - See more at: http://www.codemiles.com/c-opengl-examples/draw-3d-cube-using-opengl-t9018.html#sthash.uD5lm0OP.dpuf
-		
+	    
+	    
 	}
 	
 	@Override
@@ -66,10 +77,19 @@ public class GUIMainMenu extends GUI{
 		{
 		case Keyboard.KEY_A:
 			ballPos.setX(ballPos.getX() + 1);
-			System.out.println("this");
+			System.out.println(XAccel);
 			return 3;
 		case Keyboard.KEY_D:
 			ballPos.setX(ballPos.getX() - 1);
+			System.out.println(XAccel);
+			return 3;
+		case Keyboard.KEY_W:
+			ballPos.setY(ballPos.getY() + 1);
+			System.out.println(YAccel);
+			return 3;
+		case Keyboard.KEY_S:
+			ballPos.setY(ballPos.getY() - 1);
+			System.out.println(YAccel);
 			return 3;
 		}
 		return 0;
@@ -83,14 +103,32 @@ public class GUIMainMenu extends GUI{
 		switch(keyID)
 		{
 		case Keyboard.KEY_A:
-			ballPos.setX(ballPos.getX() + 1*(XAccel++));	// temporary acceleration
-			System.out.println("this");
+			//ballPos.setX(ballPos.getX() + 1*(XAccel++));	// temporary acceleration
+			ballPos.setX(ballPos.getX() + 1*(XAccel));		// w/o accel
+			System.out.println(XAccel);
+			//needToSlowDown = true;
 			return 3;
 		case Keyboard.KEY_D:
-			ballPos.setX(ballPos.getX() + 1*(XAccel--));	// temporary acceleration
+			//ballPos.setX(ballPos.getX() + 1*(XAccel--));	// temporary acceleration
+			ballPos.setX(ballPos.getX() - 1*(XAccel));		// w/o accel
+			System.out.println(XAccel);
+			//needToSlowDown = true;
+			return 3;
+		case Keyboard.KEY_W:
+			//ballPos.setY(ballPos.getY() + 1*(YAccel++));
+			ballPos.setY(ballPos.getY() + 1*(YAccel));		
+			System.out.println(YAccel);
+			return 3;
+		case Keyboard.KEY_S:
+			//ballPos.setY(ballPos.getY() + 1*(YAccel--));
+			ballPos.setY(ballPos.getY() - 1*(YAccel));
+			System.out.println(YAccel);
 			return 3;
 		}
+		
 		return 0;
 	}
+	
+	
 
 }
